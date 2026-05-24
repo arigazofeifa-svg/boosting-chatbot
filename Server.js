0abf02ded8f7a14739cc95b1ca5c2c2a8609ea51
arigazofeifa-svg <arigazofeifa@gmail.com>
2026-05-24 16:53:44 -0600

    Update nodemailer transporter configuration
    Remove email authentication details from transporter configuration.

--- Server.js
+++ Server.js
@@ -8,14 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.static(__dirname));
 app.use(express.json());
-
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+  family: 4,
   },
   tls: {
     rejectUnauthorized: false
